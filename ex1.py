f6bc7901023d379b04f31320739f5a22f883a98c
alvardhovakimyan <152319655+alvardhovakimyan@users.noreply.github.com>
2023-11-28 22:57:00 +0400

    Update ex1.py

--- ex1.py
+++ ex1.py
@@ -6,8 +6,10 @@ Divisible by 5
 55
 
 '''
-inputList=[10, 20, 33, 46, 55]
+inputList=[
+    10, 20, 33,
+    46, 55]
 print("Divisible by 5")
 for i in inputList:
-    if i%5 ==0:
+    if i % 5 == 0:
         print(i)
