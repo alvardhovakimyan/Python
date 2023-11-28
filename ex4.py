619c15187b52009d0b1914394626a32853256c81
alvardhovakimyan <152319655+alvardhovakimyan@users.noreply.github.com>
2023-11-28 22:57:31 +0400

    Update ex4.py

--- ex4.py
+++ ex4.py
@@ -11,5 +11,5 @@ Print the following pattern
 number = int(input("Enter a number: "))
 for i in range(1, number+1):
     for j in range(i):
-        print(i, end=" ")
+        print(i, end = " ")
     print()
