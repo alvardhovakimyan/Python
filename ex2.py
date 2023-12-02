86a569dec30bd60f90fb1da7a98175bb8199f962
alvardhovakimyan <152319655+alvardhovakimyan@users.noreply.github.com>
2023-12-02 20:28:22 +0400

    Update ex2.py

--- ex2.py
+++ ex2.py
@@ -3,6 +3,15 @@ ex.2
 Remove first n characters from a string
 Write a program to remove characters from a string starting from zero up to n and return a new string.
 '''
-n=int(input("enter a number: "))
-text="return a new string"
-print(text[n:])
+
+text = input("Input text: ")
+while True:
+  num = input("enter a number: ")
+  if num.isdigit():
+    if num > 0 or num = 0:
+      print(text[num:])
+      break
+    else:
+      print("Please enter a positive integer.")
+  else:
+    print("Invalid input. Please enter a valid number.")
