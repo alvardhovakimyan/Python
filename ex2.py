042255aea5946532eb0d00926d888be3172f40a3
alvardhovakimyan <152319655+alvardhovakimyan@users.noreply.github.com>
2023-12-02 20:30:13 +0400

    Update ex2.py

--- ex2.py
+++ ex2.py
@@ -6,12 +6,13 @@ Write a program to remove characters from a string starting from zero up to n an
 
 text = input("Input text: ")
 while True:
-  num = input("enter a number: ")
-  if num.isdigit():
-    if num > 0 or num = 0:
-      print(text[num:])
-      break
+    num = input("Enter a number: ")
+    if num.isdigit():
+        num = int(num)
+        if num >= 0:
+            print(text[num:])
+            break
+        else:
+            print("Please enter a non-negative integer.")
     else:
-      print("Please enter a positive integer.")
-  else:
-    print("Invalid input. Please enter a valid number.")
+        print("Invalid input. Please enter a valid number.")
