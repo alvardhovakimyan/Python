0fbe8d38666b8f9ed7e8cc346588812dbff63650
alvardhovakimyan <152319655+alvardhovakimyan@users.noreply.github.com>
2023-12-02 20:38:31 +0400

    Update ex8.py

--- ex8.py
+++ ex8.py
@@ -3,6 +3,8 @@ ex8
 write functions for the following string operations
 endswith(str_obj, sub_string)
 '''
+
+
 def ends_with(str_obj, sub_string):
     return str_obj[-len(sub_string):] == sub_string
 string_obj = str(input("Enter your text: "))
