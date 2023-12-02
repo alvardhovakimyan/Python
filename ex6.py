e7be8644d19e124a0e6e8dc255a958ccdac3574f
alvardhovakimyan <152319655+alvardhovakimyan@users.noreply.github.com>
2023-12-02 20:41:58 +0400

    Update ex6.py

--- ex6.py
+++ ex6.py
@@ -2,7 +2,10 @@
 ex.6
 write a function that returns the number of non-overlapping occurrences of substring sub in string S
 i.e. write a function that does the same as a builtin str.count() function does
-function signature: count_sub_in_str(string_obj, substring)'''
+function signature: count_sub_in_str(string_obj, substring)
+'''
+
+
 def count_sub_in_str(string_obj, substring):
     count = 0
     len_substring = len(substring)
